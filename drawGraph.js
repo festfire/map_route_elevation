8ea3c8e1753690e5179a672590b71f2445b36a75
mortuspoe <wd778@ya.ru>
2018-07-07 15:31:08 +0300

    Add files via upload

--- drawGraph.js
+++ drawGraph.js
@@ -1,19 +1,22 @@
-function drawGraph(data, distance) {
+function drawGraph(canvas, data, distance) {
 	/*var data = [
 		{alt: 10}, {alt: 20}, {alt: 30}, {alt: 40}, {alt: 15}, {alt: 25}, {alt: 10}, {alt: 20}, {alt: 30}, {alt: 40}, {alt: 15}, {alt: 25}, {alt: 10}, {alt: 20}, {alt: 30}, {alt: 40}, {alt: 15}, {alt: 25}, {alt: 10}, {alt: 20}, {alt: 30}, {alt: 40}, {alt: 15}, {alt: 25}, 
 	]*/;
-	console.log(distance);
 
 
-	var ctx = document.getElementById('canvas').getContext('2d');
+	var ctx = canvas;//document.getElementById('canvas').getContext('2d');
 	
 
-	var altData = [];
+	var altDataUp = [];
+	var altDataDown = [];
+	var altDataFlat = [];
 	var labels = [];
+	var colors = [];
 	var min = 10000;
 	var max = -10000;
+	var lastAlt = 0;
 	data.forEach(function(item, i, arr) {
-		altData.push(item.alt);
+		//altData.push(item.alt);
 		labels.push((i / (data.length - 1) * distance).toFixed(0));
 		if (item.alt > max) {
 			max = item.alt;
@@ -21,70 +24,148 @@ function drawGraph(data, distance) {
 		if (item.alt < min) {
 			min = item.alt;
 		}
+		if (i > 0) {
+			var angle = Math.atan2(item.alt - lastAlt, distance / data.length);
+			colors.push(Math.abs(angle) > 1 ? (angle >= 0 ? 1 : -1) : 0);
+		}
+		lastAlt = item.alt;
+	});
+
+	data.forEach(function(item, i, arr) {
+		switch(colors[i]) {
+			case -1:
+				altDataDown.push(item.alt);
+				altDataFlat.push(null);
+				altDataUp.push(null);
+			break;
+			case 0:
+				altDataDown.push(null);
+				altDataFlat.push(item.alt);
+				altDataUp.push(null);
+			break;
+			case 1:
+				altDataDown.push(null);
+				altDataFlat.push(null);
+				altDataUp.push(item.alt);
+			break;
+		}
+
+		if (colors[i] != colors[i-1]) {
+			switch(colors[i-1]) {
+				case -1:
+					altDataDown[i] = item.alt;
+				break;
+				case 0:
+					altDataFlat[i] = item.alt;
+				break;
+				case 1:
+					altDataUp[i] = item.alt;
+				break;
+			}			
+		}
+	});
+
+	data.forEach(function(item, i, arr) {
+
 	});
 
 	var lowScale = min - (max - min) * .2;
 	var highScale = max + (max - min) * .2;
+	var minDist = 200;
 	lowScale = Math.max(0, lowScale);
 
-	var chart = new Chart(ctx, {
-	    // The type of chart we want to create
-	    type: 'line',
+	if (highScale - lowScale < minDist) {
+		highScale += ((minDist - (highScale - lowScale)) / 2);
+		lowScale -= ((minDist - (highScale - lowScale)) / 2);	
+	}
 
-	    // The data for our dataset
-	    data: {
-	        labels: labels,//["January", "February", "March", "April", "May", "June", "July"],
-	        datasets: [{
-	            label: "Altitude profile",
-	            /*backgroundColor: 
-	            
-		            pattern.draw('square', '#ff6384'),
-		            pattern.draw('circle', '#36a2eb'),
-		            pattern.draw('diamond', '#cc65fe'),
-		            pattern.draw('triangle', '#ffce56'),
-		        ],*/
-		        backgroundColor: 'rgb(232, 199, 255)',
-	            borderColor: 'rgb(132, 99, 255)',
-	            data: altData,//[1100, 1110, 1115, 1112, 1120, 1130, 1145],
-	        }]
-	    },
+	if (lowScale < 0) {
+		highScale -= lowScale;
+		lowScale = 0;
+	}
 
-	    // Configuration options go here
-	    options: {
-	    	responsive: true,
-	    	maintainAspectRatio: false,
-	    	legend: {
-	    		display: false,
-	    	},
-	    	title: {
-	    		display: false,
-	    		text: 'Altitude profile'
-	    	},
-	    	scales: {
-	    		yAxes: [{
-	    			ticks: {
-	    				min: lowScale,
-	    				max: highScale,
-	    			},
-	    			scaleLabel: {
-		    			display: true,
-		    			labelString: 'Altitude, m',
-		    			fontStyle: 'bold',
-		    			fontSize: 16
-		    		}
-	    		}],
-	    		xAxes: [{
-	    			ticks: {
-	    				stepSize: 5
-	    			},
-	    			scaleLabel: {
-		    			display: true,
-		    			labelString: 'Distance, m',
-		    			fontStyle: 'bold',
-		    			fontSize: 16
-		    		}
-	    		}],
-	    	}
-	    }
-	});
+	var img = new Image();
+	
+	var fillPattern;
+	img.onload = function() {
+		fillPattern = ctx.createPattern(img, 'repeat');
+
+		var chart = new Chart(ctx, {
+		    // The type of chart we want to create
+		    type: 'line',
+
+		    // The data for our dataset
+		    data: {
+		        labels: labels,//["January", "February", "March", "April", "May", "June", "July"],
+		        datasets: [{
+		            label: "Going down",
+			        backgroundColor: 'rgb(199, 255, 199)',
+		            borderColor: 'rgb(99, 255, 99)',
+		            lineTension: 0,
+		            data: altDataDown,
+		        },
+		        {
+		            label: "Going flat",
+			        backgroundColor: 'rgb(232, 199, 255)',
+		            borderColor: 'rgb(132, 99, 255)',
+		            lineTension: 0,
+		            data: altDataFlat,
+		        },
+		        {
+		            label: "Going up",
+			        backgroundColor: 'rgb(255, 199, 199)',
+		            borderColor: 'rgb(255, 99, 99)',
+		            lineTension: 0,
+		            data: altDataUp,
+		        },
+		        ]
+		    },
+
+		    // Configuration options go here
+		    options: {
+		    	responsive: true,
+		    	maintainAspectRatio: false,
+		    	legend: {
+		    		display: false,
+		    	},
+		    	title: {
+		    		display: false,
+		    		text: 'Altitude profile'
+		    	},
+		    	scales: {
+		    		yAxes: [{
+		    			ticks: {
+		    				min: lowScale,
+		    				max: highScale,
+		    			},
+		    			scaleLabel: {
+			    			display: true,
+			    			labelString: 'Altitude, m',
+			    			fontStyle: 'bold',
+			    			fontSize: 16
+			    		}
+		    		}],
+		    		xAxes: [{
+		    			ticks: {
+		    				stepSize: 5
+		    			},
+		    			scaleLabel: {
+			    			display: true,
+			    			labelString: 'Distance, m',
+			    			fontStyle: 'bold',
+			    			fontSize: 16
+			    		}
+		    		}],
+		    	}
+		    }
+		});
+/*
+		setTimeout(function() {
+			ctx.fillStyle = "rgba(0, 255, 0, .25)";
+			ctx.fillRect(0, 0, 100, 100);
+		}, 1000);*/
+	};
+
+	img.src = 'test.png';
+	
 }
